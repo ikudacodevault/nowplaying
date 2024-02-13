0c22467fce3f21726aa77c109d3a6cfe9e2770a2
Ace Ikuda <149534225+aceikuda@users.noreply.github.com>
2024-02-13 21:19:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var playerElements = {
+  song: document.getElementById("player-song"),
+  artist: document.getElementById("player-artist"),
+  status: document.getElementById("player-status"),
+  albumArt: document.getElementById("player-album-art"),
+  progress: document.getElementById("player-progress"),
+  background: document.getElementById("player-background"),
+  songLink: document.getElementById("player-song-link"),
+  artistLink: document.getElementById("player-artist-link"),
+  albumLink: document.getElementById("player-album-link"),
+  time: document.getElementById("player-time")
+};
+
+var songData, progressSeconds, totalSeconds, progressInterval;
+
+function updatePlayer() { // Replace the URL below with the one to your worker
+  fetch(`https://ikudanet.github.io/nowplaying/example-api-response.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.hasOwnProperty("NOTICE")) { // You can replace "User" with your name / username
+        playerElements.song.innerHTML = `User isn't playing anything.`;
+        playerElements.artist.innerHTML = "";
+        return;
+      }
+      songData = data;
+      playerElements.song.innerHTML = data.item.name;
+      playerElements.artist.innerHTML = data.item.artists[0].name;
+      playerElements.status.innerHTML = data.is_playing ? `▶️ Playing` : `⏸ Paused`;
+      playerElements.albumArt.setAttribute("src", data.item.album.images[0].url);
+      playerElements.background.style.backgroundImage = `url(${data.item.album.images[0].url})`;
+
+      playerElements.songLink.setAttribute("href", data.item.external_urls.spotify);
+      playerElements.artistLink.setAttribute("href", data.item.artists[0].external_urls.spotify);
+      playerElements.albumLink.setAttribute("href", data.item.album.external_urls.spotify);
+
+      progressSeconds = Math.ceil(data.progress_ms / 1000);
+      totalSeconds = Math.ceil(data.item.duration_ms / 1000);
+      clearInterval(progressInterval);
+      if (data.is_playing) {
+        progressInterval = setInterval(setProgress, 1000);
+      } else {
+        setProgress();
+      }
+    });
+}
+
+function setProgress() {
+  if (progressSeconds > totalSeconds) {
+    clearInterval(progressInterval);
+    updatePlayer();
+    return;
+  }
+  ++progressSeconds;
+  var totalLabel = pad(parseInt(totalSeconds / 60)) + ":" + pad(totalSeconds % 60);
+  var progressLabel = pad(parseInt(progressSeconds / 60)) + ":" + pad(progressSeconds % 60);
+  playerElements.time.innerHTML = progressLabel + " / " + totalLabel;
+  playerElements.progress.style.width = `${(progressSeconds * 100) / totalSeconds}%`;
+}
+
+function pad(val) {
+  return val.toString().padStart(2, '0');
+}
+
+updatePlayer();
+setInterval(updatePlayer, 30000); // Refreshes the player (a.k.a checks API) every 30 seconds, lower or higher the rate as you see fit
---
+++ worker.js
@@ -0,0 +1,128 @@
+// The "API"
+// CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN are environment variables defined in your Cloudflare Worker's settings.
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleNowPlaying(event.request));
+});
+
+function deepRemoveKeys(obj, keysToRemove) { // This function (and the helpful comments) were by ChatGPT, because I'm sleep-deprived and could not figure this out. I did have a badly working version at some point though :)
+  let modified = false;
+
+  // Helper function to recursively remove keys
+  function removeKeys(obj) {
+    if (Array.isArray(obj)) {
+      // If obj is an array, recursively call removeKeys on each element of the array
+      for (let i = obj.length - 1; i >= 0; i--) {
+        if (typeof obj[i] === 'object') {
+          obj[i] = removeKeys(obj[i]);
+        }
+      }
+    } else if (typeof obj === "object" && obj !== null) {
+      // If obj is an object, iterate over its keys
+      for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          // Check if the key should be removed
+          if (keysToRemove.includes(key)) {
+            delete obj[key];
+            modified = true;
+          } else {
+            // If the key should not be removed, check if the value is an object
+            // and recursively call removeKeys on it
+            if (typeof obj[key] === 'object') {
+              obj[key] = removeKeys(obj[key]);
+            }
+          }
+        }
+      }
+    }
+    return obj;
+  }
+
+  // Call removeKeys until no more modifications are made
+  do {
+    modified = false;
+    obj = removeKeys(obj);
+  } while (modified);
+
+  return obj;
+}
+
+async function handleNowPlaying(request) {
+  var access_token = await fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    headers: {
+      Authorization: // The ts-ignores stop VS Code from spitting out errors stating the variables cannot be found
+        // @ts-ignore
+        "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    // @ts-ignore
+    body: `grant_type=refresh_token&refresh_token=${REFRESH_TOKEN}`,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data.access_token;
+    });
+
+  var songData = await fetch(
+    "https://api.spotify.com/v1/me/player/currently-playing",
+    {
+      headers: {
+        Authorization: "Bearer " + access_token,
+      },
+    }
+  )
+    .then((response) => response.json()) // Could be unnecessary, I just wanted to clean up the API response, because... why not?
+    .then((data) => {
+      return deepRemoveKeys(data, [
+        "available_markets",
+        "preview_url",
+        "context", // NOTE: Context reveals the URL of the playlist you're playing a song from!
+        "external_ids",
+        "release_date",
+        "release_date_precision",
+        "total_tracks",
+        "popularity",
+        "total_tracks",
+        "explicit",
+        "disc_number",
+        "track_number",
+        "uri",
+        "album_type",
+        "href",
+        "currently_playing_type",
+        "actions",
+        "type",
+        "id"
+      ]);
+    })
+    .then((data) => JSON.stringify(data))
+    .catch((error) => {
+      console.error("Error fetching data from Spotify's API:", error);
+      return null;
+    });
+
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "set this to https://yourdomain.tld or allow all with: *",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Max-Age": "86400",
+    "X-Robots-Tag": "noindex", // Tells search engines not to index API responses
+    "Cache-Control": "no-cache, no-store, must-revalidate", // I added this to ensure the client always receives the uncached, latest API response
+  };
+
+  if (songData === null || Object.keys(songData).length === 0) {
+    const noticeResponse = {
+      NOTICE:
+        "Couldn't retrieve data from Spotify's API. This either means that music isn't being played, or that the API is down.",
+    };
+    return new Response(JSON.stringify(noticeResponse), { headers: corsHeaders });
+  }
+
+  songData = JSON.parse(songData);
+
+  return new Response(JSON.stringify(songData, null, 2), {
+    headers: corsHeaders,
+  });
+}
